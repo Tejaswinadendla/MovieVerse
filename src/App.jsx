09e665256b13dbdb8602d1674bf66fee51762fb9
Tejaswinadendla <saitejaswinadendla045@gmail.com>
2025-11-21 16:05:26 +0530

    Initial commit - MovieVerse project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Home from './pages/Home';
+import './css/App.css'
+import { Route,Routes } from 'react-router-dom';
+import Favorites from './pages/Favorites';
+import NavBar from './components/NavBar';
+import { MovieProvider } from './MovieContext';
+
+function App() {
+  
+  return (
+    <MovieProvider>
+      <NavBar/>
+    <main className='main-content'>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/favorites" element={<Favorites/>}/>
+      </Routes>
+    </main>
+    </MovieProvider>
+    
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,50 @@
+import MovieCard from "../components/MovieCard";
+import { useState ,useEffect} from "react";
+import { searchMovies , getPopularMovies} from "../services/api";
+import "../css/Home.css";
+function Home(){
+    const[searchQuery, setSearchQuery]=useState("");
+
+    const [movies,setMovies]=useState([]);
+    const [error,setError]=useState(null);
+    const[loading, setLoading]=useState(true);
+
+    useEffect(()=>{
+        const loadPopularMovies=async()=>{
+            try{
+                const popularMovies=await getPopularMovies();
+                setMovies(popularMovies);
+            }
+            catch (err){
+                setError("failed to load");
+            }
+            finally {
+                setLoading(false);
+            }
+        };
+        loadPopularMovies();
+    },[]);
+
+   
+    const handleSearch=(e)=>{
+        e.preventDefault();
+        alert(searchQuery);
+        setSearchQuery("");
+    };
+
+    return(
+        <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input type="text" placeholder="serach here" className="search-input" value={searchQuery} onChange={(e)=>setSearchQuery(e.target.value)} />
+                <button type="submit" className="search-button">Search</button>
+            </form>
+            {loading? (<div className="loading">Loading...</div> ):(<div className="movies-grid">{movies.map((movie)=> movie.title.toLowerCase().startsWith(searchQuery) && (<MovieCard movie={movie} key={movie.id} />))}</div>)}
+            <div className="movies-grid">{movies.map((movie)=> movie.title.toLowerCase().startsWith(searchQuery) && (<MovieCard movie={movie} key={movie.id} />))}</div>
+        </div>
+    );
+}
+export default Home;
+
+
+
+
